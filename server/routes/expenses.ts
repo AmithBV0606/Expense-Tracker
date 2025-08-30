392358d7ad7188bc00013a28fc3a52dfd03b83dd
Amith B V <amithrao0606@gmail.com>
2025-08-30 23:24:59 +0530

    Added Fake data.

--- server/routes/expenses.ts
+++ server/routes/expenses.ts
@@ -1,9 +1,36 @@
 import { Hono } from "hono";
 
+type Expense = {
+  id: number;
+  title: string;
+  amount: number;
+};
+
+const fakeExpenses: Expense[] = [
+  {
+    id: 1,
+    title: "Coffee at Starbucks",
+    amount: 4.5,
+  },
+  {
+    id: 2,
+    title: "Groceries - Trader Joe's",
+    amount: 56.2,
+  },
+  {
+    id: 3,
+    title: "Monthly Netflix Subscription",
+    amount: 15.99,
+  },
+];
+
 export const expensesRoute = new Hono()
   .get("/", (c) => {
-    return c.json({ expenses: [] });
+    return c.json({ expenses: fakeExpenses });
   })
-  .post("/", (c) => {
-    return c.json({});
+  .post("/", async (c) => {
+    const data: Expense = await c.req.json();
+    console.log(data.amount);
+    console.log(data);
+    return c.json(data);
   });
