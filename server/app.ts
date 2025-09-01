2ca602c86140f8e1443e61d5e18c3fc5fec244e8
Amith B V <amithrao0606@gmail.com>
2025-09-01 07:41:38 +0530

    Built the app inorder to serve the static files of frontend from the backend.

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { expensesRoute } from "./routes/expenses";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
@@ -8,9 +9,13 @@ const app = new Hono();
 app.use(logger());
 
 // Starting endpoint :
-app.get("/", (c) => c.text("Hello from Hono!"));
+// app.get("/", (c) => c.text("Hello from Hono!"));
 
 // Expenses endpoint :
 app.route("/api/expenses", expensesRoute);
+
+// Serving Static files :
+app.get("*", serveStatic({ root: "./client/dist" }));
+app.get("*", serveStatic({ path: "./client/dist/index.html" }));
 
 export default app;
