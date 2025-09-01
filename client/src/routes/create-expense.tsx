5d29d78db9b1e9ac3f4b5fddd3f3600ed6ebadcb
Amith B V <amithrao0606@gmail.com>
2025-09-01 20:29:52 +0530

    Integrated Tanstack Form.

--- client/src/routes/create-expense.tsx
+++ client/src/routes/create-expense.tsx
@@ -1,31 +1,102 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import { useForm } from "@tanstack/react-form";
+import type { AnyFieldApi } from "@tanstack/react-form";
+import { api } from "@/lib/api";
 
 export const Route = createFileRoute("/create-expense")({
   component: CreateExpense,
 });
 
+function FieldInfo({ field }: { field: AnyFieldApi }) {
+  return (
+    <>
+      {field.state.meta.isTouched && !field.state.meta.isValid ? (
+        <em>{field.state.meta.errors.join(", ")}</em>
+      ) : null}
+      {field.state.meta.isValidating ? "Validating..." : null}
+    </>
+  );
+}
+
 function CreateExpense() {
+  const navigate = useNavigate();
+
+  const form = useForm({
+    defaultValues: {
+      title: "",
+      amount: 0,
+    },
+    onSubmit: async ({ value }) => {
+      await new Promise((r) => setTimeout(r, 2000));
+      const res = await api.expenses.$post({ json: value });
+      if (!res.ok) {
+        throw new Error("Server error!!");
+      }
+      navigate({ to: "/expenses" });
+    },
+  });
+
   return (
     <div className="p-2 space-y-3">
       <Card className="max-w-2xl m-auto px-8">
         <h2 className=" max-w-xl m-auto">Create Expenses</h2>
 
-        <form action="" className="space-y-3">
-          <div className="grid w-full max-w-xl items-center gap-3">
-            <Label htmlFor="title">Title</Label>
-            <Input type="text" id="title" placeholder="Title" />
-          </div>
+        <form
+          action=""
+          className="space-y-3"
+          onSubmit={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            form.handleSubmit();
+          }}
+        >
+          <form.Field
+            name="title"
+            children={(field) => (
+              <div className="grid w-full max-w-xl items-center gap-3">
+                <Label htmlFor={field.name}>Title</Label>
+                <Input
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+                <FieldInfo field={field} />
+              </div>
+            )}
+          />
 
-          <div className="grid w-full max-w-xl items-center gap-3">
-            <Label htmlFor="amount">Amount</Label>
-            <Input type="number" id="amount" placeholder="Amount" />
-          </div>
+          <form.Field
+            name="amount"
+            children={(field) => (
+              <div className="grid w-full max-w-xl items-center gap-3">
+                <Label htmlFor={field.name}>Amount</Label>
+                <Input
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  type="number"
+                  onChange={(e) => field.handleChange(Number(e.target.value))}
+                />
+                <FieldInfo field={field} />
+              </div>
+            )}
+          />
 
-          <Button type="submit">Create Expense</Button>
+          <form.Subscribe
+            selector={(state) => [state.canSubmit, state.isSubmitting]}
+            children={([canSubmit, isSubmitting]) => (
+              <Button type="submit" disabled={!canSubmit}>
+                {isSubmitting ? "..." : "Create Expense"}
+              </Button>
+            )}
+          />
         </form>
       </Card>
     </div>
