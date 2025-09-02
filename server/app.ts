00b18545112941c04f71264482010c8d0bb7c9d4
Amith B V <amithrao0606@gmail.com>
2025-09-02 08:21:48 +0530

    Kinde Auth setup completed.

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { expensesRoute } from "./routes/expenses";
 import { serveStatic } from "hono/bun";
+import { authRoute } from "./routes/auth";
 
 const app = new Hono();
 
@@ -12,7 +13,10 @@ app.use(logger());
 // app.get("/", (c) => c.text("Hello from Hono!"));
 
 // Expenses endpoint :
-const apiRoutes = app.basePath("/api").route("/expenses", expensesRoute);
+const apiRoutes = app
+  .basePath("/api")
+  .route("/expenses", expensesRoute)
+  .route("/", authRoute);
 
 // Serving Static files :
 app.get("*", serveStatic({ root: "./client/dist" }));
