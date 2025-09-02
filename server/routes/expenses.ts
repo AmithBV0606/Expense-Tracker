4b179be159afcee74a4f60fd9a8ac4bae105cfc2
Amith B V <amithrao0606@gmail.com>
2025-09-02 13:38:01 +0530

    Drizzle ORM setup.

--- server/routes/expenses.ts
+++ server/routes/expenses.ts
@@ -2,11 +2,14 @@ import { Hono } from "hono";
 import { z } from "zod";
 import { zValidator } from "@hono/zod-validator";
 import { getUser } from "../kinde";
+import { db } from "../db";
+import { expenses as expensesTable } from "../db/schema/expenses";
+import { eq } from "drizzle-orm";
 
 const expenseSchema = z.object({
   id: z.number().int().positive().min(1),
   title: z.string().min(5).max(100),
-  amount: z.number().int().positive(),
+  amount: z.string(),
 });
 
 type Expense = z.infer<typeof expenseSchema>;
@@ -17,34 +20,49 @@ const fakeExpenses: Expense[] = [
   {
     id: 1,
     title: "Coffee at Starbucks",
-    amount: 4.5,
+    amount: "4.5",
   },
   {
     id: 2,
     title: "Groceries - Trader Joe's",
-    amount: 56.2,
+    amount: "56.2",
   },
   {
     id: 3,
     title: "Monthly Netflix Subscription",
-    amount: 15.99,
+    amount: "15.99",
   },
 ];
 
 export const expensesRoute = new Hono()
-  .get("/", getUser, (c) => {
+  .get("/", getUser, async (c) => {
     const user = c.var.user;
-    return c.json({ expenses: fakeExpenses });
+
+    const expenses = await db
+      .select()
+      .from(expensesTable)
+      .where(eq(expensesTable.userId, user.id));
+
+    return c.json({ expenses: expenses });
   })
   .post("/", getUser, zValidator("json", createPostSchema), async (c) => {
-    const data = await c.req.valid("json");
-    fakeExpenses.push({ id: fakeExpenses.length + 1, ...data });
+    const expense = await c.req.valid("json");
+    const user = c.var.user;
+
+    const result = await db
+      .insert(expensesTable)
+      .values({
+        ...expense,
+        userId: user.id,
+      })
+      .returning();
+
     c.status(201);
-    return c.json(data);
+    return c.json(result);
   })
   .get("/total-spent", getUser, (c) => {
     const total = fakeExpenses.reduce(
-      (acc, expense) => acc + expense.amount,
+      (acc, expense) => acc + +expense.amount,
       0
     );
     c.status(200);
