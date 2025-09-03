a7b84b439f2486b2e067cd7550f04bd2d4410ba7
Amith B V <amithrao0606@gmail.com>
2025-09-03 12:43:45 +0530

    UI Changes.

--- client/src/routes/_authenticated/create-expense.tsx
+++ client/src/routes/_authenticated/create-expense.tsx
@@ -2,7 +2,6 @@ import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
 import { useForm } from "@tanstack/react-form";
 import type { AnyFieldApi } from "@tanstack/react-form";
 import { api } from "@/lib/api";
@@ -43,91 +42,93 @@ function CreateExpense() {
 
   return (
     <div className="p-2 space-y-3">
-      <Card className="max-w-2xl m-auto px-8">
-        <h2 className=" max-w-xl m-auto">Create Expenses</h2>
+      <h1 className="font-bold">Create Expenses</h1>
 
-        <form
-          action=""
-          className="space-y-3"
-          onSubmit={(e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            form.handleSubmit();
+      <form
+        action=""
+        className="flex flex-col gap-y-4 max-w-xl m-auto"
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          form.handleSubmit();
+        }}
+      >
+        <form.Field
+          name="title"
+          validators={{
+            onChange: ({ value }) =>
+              value.length < 3
+                ? "Title must contain at least 3 characters!!"
+                : undefined,
           }}
-        >
-          <form.Field
-            name="title"
-            validators={{
-              onChange: ({ value }) =>
-                value.length < 3
-                  ? "Title must contain at least 3 characters!!"
-                  : undefined,
-            }}
-            children={(field) => (
-              <div className="grid w-full max-w-xl items-center gap-3">
-                <Label htmlFor={field.name}>Title</Label>
-                <Input
-                  id={field.name}
-                  name={field.name}
-                  value={field.state.value}
-                  onBlur={field.handleBlur}
-                  onChange={(e) => field.handleChange(e.target.value)}
-                />
-                <FieldInfo field={field} />
-              </div>
-            )}
-          />
+          children={(field) => (
+            <div className="grid w-full max-w-xl items-center gap-3">
+              <Label htmlFor={field.name}>Title</Label>
+              <Input
+                id={field.name}
+                name={field.name}
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onChange={(e) => field.handleChange(e.target.value)}
+              />
+              <FieldInfo field={field} />
+            </div>
+          )}
+        />
 
-          <form.Field
-            name="amount"
-            validators={{
-              onChange: ({ value }) =>
-                Number(value) < 0 ? "Amount must be positive!!" : undefined,
-            }}
-            children={(field) => (
-              <div className="grid w-full max-w-xl items-center gap-3">
-                <Label htmlFor={field.name}>Amount</Label>
-                <Input
-                  id={field.name}
-                  name={field.name}
-                  value={field.state.value}
-                  onBlur={field.handleBlur}
-                  type="number"
-                  onChange={(e) => field.handleChange(e.target.value)}
-                />
-                <FieldInfo field={field} />
-              </div>
-            )}
-          />
+        <form.Field
+          name="amount"
+          validators={{
+            onChange: ({ value }) =>
+              Number(value) < 0 ? "Amount must be positive!!" : undefined,
+          }}
+          children={(field) => (
+            <div className="grid w-full max-w-xl items-center gap-3">
+              <Label htmlFor={field.name}>Amount</Label>
+              <Input
+                id={field.name}
+                name={field.name}
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                type="number"
+                onChange={(e) => field.handleChange(e.target.value)}
+              />
+              <FieldInfo field={field} />
+            </div>
+          )}
+        />
 
-          <form.Field
-            name="date"
-            children={(field) => (
-              <div className="self-center m-auto w-full">
-                <Calendar
-                  mode="single"
-                  selected={new Date(field.state.value)}
-                  onSelect={(date) =>
-                    field.handleChange((date ?? new Date()).toISOString())
-                  }
-                  className="rounded-md border shadow-sm"
-                  captionLayout="dropdown"
-                />
-                <FieldInfo field={field} />
-              </div>
-            )}
-          />
+        <form.Field
+          name="date"
+          validators={{
+            onChange: ({ value }) =>
+              value === null ? "Please enter the date!!" : undefined,
+          }}
+          children={(field) => (
+            <div className="self-center grid max-w-2xl items-center gap-3">
+              <Calendar
+                mode="single"
+                selected={new Date(field.state.value)}
+                onSelect={(date) =>
+                  field.handleChange((date ?? new Date()).toISOString())
+                }
+                className="rounded-md border shadow-sm"
+                captionLayout="dropdown"
+              />
+              <FieldInfo field={field} />
+            </div>
+          )}
+        />
 
-          <form.Subscribe
-            selector={(state) => [state.canSubmit, state.isSubmitting]}
-            children={([canSubmit, isSubmitting]) => (
-              <Button type="submit" disabled={!canSubmit}>
-                {isSubmitting ? "..." : "Create Expense"}
-              </Button>
-            )}
-          />
-        </form>
-      </Card>
+        <form.Subscribe
+          selector={(state) => [state.canSubmit, state.isSubmitting]}
+          children={([canSubmit, isSubmitting]) => (
+            <Button type="submit" disabled={!canSubmit}>
+              {isSubmitting ? "..." : "Create Expense"}
+            </Button>
+          )}
+        />
+      </form>
     </div>
   );
 }
